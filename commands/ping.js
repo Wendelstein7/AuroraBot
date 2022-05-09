ee9d525533f399836af661e62d1084a290570b5d
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2022-05-09 23:48:23 +0200

    Ping times in milliseconds and emoji

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,31 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+const emojis = [
+  { ms: 200, emoji: ":grin:" },
+  { ms: 350, emoji: ":relaxed:" },
+  { ms: 500, emoji: ":slight_smile:" },
+  { ms: 750, emoji: ":neutral_face:" },
+  { ms: 1000, emoji: ":slight_frown:" },
+  { ms: 2000, emoji: ":cry:" },
+  { ms: Math.max, emoji: ":confounded:" },
+];
+
 module.exports = {
   enabled: true,
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    var start = new Date().getTime();
+
+    await interaction.reply({ content: "Pong!", ephemeral: true });
+
+    var pingMilliseconds = Math.round(new Date().getTime() - start);
+    var emoji = emojis.find((e) => pingMilliseconds < e.ms).emoji;
+
+    await interaction.editReply({
+      content: `Pong! *(took ${pingMilliseconds} ms ${emoji})*`,
+      ephemeral: true,
+    });
   },
 };
