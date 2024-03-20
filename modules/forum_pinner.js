d8f76a81c6a190c93c81e896883b82e0085666bf
Alessandro Proto <alex@alexdevs.me>
2024-03-20 21:21:16 +0100

    feat: add Forum Pinner module (#5)
    * Add Forum Pinner module to pin the first message of forum posts.
    
    * Add guard clause in `/pin` command against members without the `MANAGE_MESSAGES` permission.

---
+++ modules/forum_pinner.js
@@ -0,0 +1,78 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+async function pinPost(thread) {
+  let messages = await thread.messages.fetch();
+  let firstMessage = messages.first();
+
+  if(firstMessage.pinned)
+    return;
+
+  return await firstMessage.pin();
+}
+
+module.exports = {
+  enabled: true,
+
+  name: "Forum Pinner",
+  description: "Automatically pins the first message of a forum post for easy retrieval.",
+
+  commands: [
+    {
+      data: new SlashCommandBuilder()
+        .setName("pin")
+        .setDescription("Pin the first message of the post!"),
+
+      async execute(interaction) {
+
+        if(!interaction.memberPermissions.has("MANAGE_MESSAGES")) {
+          await interaction.reply({
+            content: `You do not have permissions to use this command!`,
+            ephemeral: true
+          });
+          return;
+        }
+
+        let channel = interaction.channel;
+
+        if (channel.type !== "GUILD_PUBLIC_THREAD") {
+          await interaction.reply({
+            content: `You need to be in a forum post!`,
+            ephemeral: true
+          });
+          return;
+        }
+
+        try {
+          await pinPost(channel);
+          await interaction.reply({
+            content: `First message (that I found) pinned!`,
+            ephemeral: true
+          });
+        } catch (e) {
+          console.error(e);
+          await interaction.reply({
+            content: `There was an error executing this command!`,
+            ephemeral: true
+          });
+        }
+      }
+    }
+  ],
+
+  events: [
+    {
+      name: "threadCreate",
+      once: false,
+      async execute(thread) {
+        if (thread.type !== "GUILD_PUBLIC_THREAD")
+          return;
+
+        try {
+          await pinPost(thread);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    }
+  ]
+};
